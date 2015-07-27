77e39ee184c10feb723daa0d3fcea5f30ecf7aa5
Gianluca <gianluca.cerminara@cern.ch>
2015-07-27 15:21:58 +0200

    add SiPixelAli to PCL workflows tested by relval 1001.0

--- Configuration/PyReleaseValidation/python/relval_steps.py
+++ Configuration/PyReleaseValidation/python/relval_steps.py
@@ -1082,7 +1082,7 @@ steps['ALCAPROMPT']={'-s':'ALCA:PromptCalibProd',
                      '--conditions':'auto:run1_data',
                      '--datatier':'ALCARECO',
                      '--eventcontent':'ALCARECO'}
-steps['ALCAEXP']={'-s':'ALCA:PromptCalibProd+PromptCalibProdSiStrip+PromptCalibProdSiStripGains',
+steps['ALCAEXP']={'-s':'ALCA:PromptCalibProd+PromptCalibProdSiStrip+PromptCalibProdSiStripGains+PromptCalibProdSiPixelAli',
                   '--conditions':'auto:run1_data',
                   '--datatier':'ALCARECO',
                   '--eventcontent':'ALCARECO'}
@@ -1138,6 +1138,11 @@ steps['ALCAHARVD3']={'-s':'ALCAHARVEST:SiStripGains',
                     '--data':'',
                     '--filein':'file:PromptCalibProdSiStripGains.root'}
 
+steps['ALCAHARVD4']={'-s':'ALCAHARVEST:SiPixelAli',
+                    '--conditions':'auto:run1_data',
+                    '--scenario':'pp',
+                    '--data':'',
+                    '--filein':'file:PromptCalibProdSiStripGains.root'}
 
 steps['RECOHISt4']=steps['RECOHI']
 
